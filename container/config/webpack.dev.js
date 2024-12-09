04a9b160795c95aa9cde3e8d6099649e04e544a6
Shum <egor@shum.dev>
2024-12-09 11:32:36 +0800

    initial commit

---
+++ container/config/webpack.dev.js
@@ -0,0 +1,32 @@
+// allows us to merge two different webpack config files
+const { merge } = require("webpack-merge");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const commonConfig = require("./webpack.common");
+
+const packg = require("../package.json");
+
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8080,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+      },
+      shared: packg.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+// devConfig will override the commonConfig properties if they collide
+module.exports = merge(commonConfig, devConfig);
---
+++ marketing/config/webpack.dev.js
@@ -0,0 +1,34 @@
+// allows us to merge two different webpack config files
+const { merge } = require("webpack-merge");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const commonConfig = require("./webpack.common");
+
+const packg = require("../package.json");
+
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8081,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      // name for the variable where this module will be stored
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+      shared: packg.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+// devConfig will override the commonConfig properties if they collide
+module.exports = merge(commonConfig, devConfig);
