bca56762e5ae4ab55d9d7d76c0f0bf89a32e1315
Shum <egor@shum.dev>
2024-12-13 12:04:08 +0800

    randomize scc class names in production

--- container/src/App.js
+++ container/src/App.js
@@ -2,18 +2,29 @@ import React from "react";
 
 import { BrowserRouter } from "react-router-dom";
 
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
 import Header from "./components/Header";
 
 import MarketingApp from "./components/MarketingApp";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
 
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
